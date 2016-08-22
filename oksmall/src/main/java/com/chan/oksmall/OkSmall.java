ae98cf0b139ebb3235772d4a7e3ff9a9aa03060d
李嘉诚 <jiacheng.li@shanbay.com>
2016-08-22 20:45:20 +0800

    update

--- oksmall/src/main/java/com/chan/oksmall/OkSmall.java
+++ oksmall/src/main/java/com/chan/oksmall/OkSmall.java
@@ -11,42 +11,27 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
-import net.wequick.small.Small;
 import net.wequick.small.util.FileUtils;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
 
 /**
  * Created by chan on 16/8/19.
  */
 public class OkSmall {
 
-    private static final String BUNDLE_MANIFEST_NAME = "bundle.json";
-    private static final String KEY_BUNDLE = "bundles";
-    private static final String KEY_URI = "uri";
-    private static final String KEY_PKG = "pkg";
     private static final String TAG = "OkSmall";
-
-    private static File sPatchManifestFile;
 
     /**
      * @param context
-     * @param pluginUri bundle.json bundles每个数组元素的uri
+     * @param pluginPackageName 插件包名
      * @param uri 远程服务器端增量包的uri
      */
-    public static void merge(Context context, String pluginUri, String uri) {
+    public static void merge(Context context, String pluginPackageName, String uri) {
 
-        String packageName = parsePackageName(context, pluginUri);
-        if (TextUtils.isEmpty(packageName)) {
+        if (TextUtils.isEmpty(pluginPackageName)) {
             Log.e(TAG, "can't parse plugin's package name, check if assets/bundle.json is deleted");
             return;
         }
@@ -54,7 +39,7 @@ public class OkSmall {
         // small框架的约定就是，插件名字是以lib+包名.so明明的
         // 比如如果我们插件的包名是com.chan.app.setting
         // 那么对于插件名就是libcom_chan_app_setting.so
-        final String soName = "lib" + packageName.replaceAll("\\.", "_") + ".so";
+        final String soName = "lib" +pluginPackageName.replaceAll("\\.", "_") + ".so";
 
         //找到旧版本的插件安装包
         final File oldPlugin = new File(context.getApplicationInfo().nativeLibraryDir, soName);
@@ -87,85 +72,5 @@ public class OkSmall {
                 YPatch.patch(oldPlugin.getAbsolutePath(), newPlugin.getAbsolutePath(), patch.getAbsolutePath());
             }
         });
-    }
-
-    private static String parsePackageName(Context context, String pluginUri) {
-        JSONObject manifest = parseManifest(context);
-        if (manifest == null) {
-            return null;
-        }
-
-        if (manifest.has(KEY_BUNDLE)) {
-            try {
-                JSONArray array = manifest.getJSONArray(KEY_BUNDLE);
-                for (int i = 0; i < array.length(); ++i) {
-                    JSONObject object = array.getJSONObject(i);
-                    if (pluginUri.equals(object.getString(KEY_URI))) {
-                        return object.getString(KEY_PKG);
-                    }
-                }
-            } catch (Exception e) {
-                return null;
-            }
-        }
-
-        return null;
-    }
-
-    private static JSONObject parseManifest(Context context) {
-
-        try {
-            File patchManifestFile = getPatchManifestFile();
-            String manifestJson = getCacheManifest();
-            if (manifestJson != null) {
-                // Load from cache and save as patch
-                if (!patchManifestFile.exists()) patchManifestFile.createNewFile();
-                PrintWriter pw = new PrintWriter(new FileOutputStream(patchManifestFile));
-                pw.print(manifestJson);
-                pw.flush();
-                pw.close();
-
-            } else if (patchManifestFile.exists()) {
-                // Load from patch
-                BufferedReader br = new BufferedReader(new FileReader(patchManifestFile));
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = br.readLine()) != null) {
-                    sb.append(line);
-                }
-
-                br.close();
-                manifestJson = sb.toString();
-            } else {
-                // Load from built-in `assets/bundle.json'
-                InputStream builtinManifestStream = context.getAssets().open(BUNDLE_MANIFEST_NAME);
-                int builtinSize = builtinManifestStream.available();
-                byte[] buffer = new byte[builtinSize];
-                builtinManifestStream.read(buffer);
-                builtinManifestStream.close();
-                manifestJson = new String(buffer, 0, builtinSize);
-            }
-
-            // Parse manifest file
-            return new JSONObject(manifestJson);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private static File getPatchManifestFile() {
-        if (sPatchManifestFile == null) {
-            synchronized (OkSmall.class) {
-                if (sPatchManifestFile == null) {
-                    sPatchManifestFile = new File(Small.getContext().getFilesDir(), BUNDLE_MANIFEST_NAME);
-                }
-            }
-        }
-        return sPatchManifestFile;
-    }
-
-    private static String getCacheManifest() {
-       return Small.getSharedPreferences().getString(BUNDLE_MANIFEST_NAME, null);
     }
 }
